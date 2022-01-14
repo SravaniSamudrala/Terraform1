7b9547a8cda7e8411553a8ad57439b570e81be8d
SravaniSamudrala <89787103+SravaniSamudrala@users.noreply.github.com>
2022-01-14 18:38:59 +0530

    Create cloudwatch.tf

---
+++ cloudwatch.tf
@@ -0,0 +1,11 @@
+resource "aws_cloudwatch_metric_alarm" "foobar" {
+  alarm_name                = "loudwatch-alarm"
+  comparison_operator       = "GreaterThanOrEqualToThreshold"
+  evaluation_periods        = "2"
+  metric_name               = "CPUUtilization"
+  namespace                 = "AWS/EC2"
+  period                    = "120"
+  statistic                 = "Average"
+  threshold                 = "80"
+  alarm_description         = "This metric monitors ec2 cpu utilization"
+  insufficient_data_actions = []
