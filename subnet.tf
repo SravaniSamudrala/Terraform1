42754059119f44e5702bcf3055623f602c0f3871
SravaniSamudrala <89787103+SravaniSamudrala@users.noreply.github.com>
2022-01-14 18:42:23 +0530

    Create subnet.tf

---
+++ subnet.tf
@@ -0,0 +1,18 @@
+resource "aws_vpc" "main" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "Sravani_VPC"
+  }
+}
+
+resource "aws_subnet" "private" {
+  count                   = 2
+  vpc_id                  = "${aws_vpc.main.id}"
+  cidr_block              = "${element(var.subnet_cidr, count.index)}"
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name        = "sravani_private"
+    Environment = "${terraform.workspace}"
